86c1a94042123449f10503968e6f7d99793133be
hp <hp@DESKTOP-C8P738T>
2020-09-03 20:59:52 +0530

    version1

---
+++ src/Master/P1.java
@@ -0,0 +1,15 @@
+package Master;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class P1 {
+	
+	@Test
+	public void test()
+	{
+		Reporter.log("Tested Successfully", true);
+	}
+	
+
+}
