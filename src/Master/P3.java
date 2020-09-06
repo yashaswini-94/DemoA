c40f1a07d091c596b7df412e1fdf7459dd3e8a34
hp <hp@DESKTOP-C8P738T>
2020-09-06 20:52:12 +0530

    Version 3

---
+++ src/Master/P3.java
@@ -0,0 +1,16 @@
+package Master;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class P3 {
+	
+		@Test
+		public void testC()
+		{
+			Reporter.log("Tested C Successfully", true);
+		}
+
+	}
+
+
